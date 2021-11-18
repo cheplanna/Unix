d4e503258fa19169f5c3115b5ce6ba1c3167f1fd
cheplanna <72358616+cheplanna@users.noreply.github.com>
2021-11-18 15:16:27 +0300

    Initial commit

---
+++ client.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+
+sock = socket.socket()
+sock.connect(('localhost', 9090))
+
+while True:
+    msg = input('Enter a message')
+    if msg =='exit':
+        print('Break connection with a server')
+        break
+    sock.send(msg.encode())
+    data = sock.recv(1024)
+    print(data.decode())
+sock.close()
